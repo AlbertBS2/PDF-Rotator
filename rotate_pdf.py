516b8d5569cd6c9ae2bc5fe0ae60b22123bdf651
AlbertBS2 <abargallos@gmail.com>
2024-12-18 02:43:19 +0100

    Feat: Add option to only rotate specific pages

--- rotate_pdf.py
+++ rotate_pdf.py
@@ -3,12 +3,17 @@ from PyPDF2 import PdfReader, PdfWriter
 # Path to the PDF file to be rotated
 dir_in = ""
 file_in = ""
-path_in = dir_in + file_in
+path_in = dir_in + "/" + file_in
 
 # Path and name for the rotated PDF file
 dir_out = ""
 file_out = file_in.replace('.pdf', '_rotated.pdf')
-path_out = dir_out + file_out
+path_out = dir_out + "/" + file_out
+
+# Pages to be rotated
+# If you want to rotate all pages, set the value to None
+# If you want to rotate only specific pages, set the value to a list of two integers [start, end]
+pages = None
 
 # Rotation angle in degrees
 deg = 90
@@ -19,9 +24,19 @@ pdf_reader = PdfReader(pdf_in)
 pdf_writer = PdfWriter()
 
 # Rotate each page in the PDF file
+i=1
 for page in pdf_reader.pages:
-    page.rotate(deg)
-    pdf_writer.add_page(page)
+    if pages is None:
+        page.rotate(deg)
+        pdf_writer.add_page(page)
+    else:
+        if i in range(pages[0], pages[1]+1):
+            page.rotate(deg)
+            pdf_writer.add_page(page)
+        else:
+            pdf_writer.add_page(page)
+    i += 1
+
 
 # Save the rotated PDF file
 pdf_out = open(path_out, 'wb')
