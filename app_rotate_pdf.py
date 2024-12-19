580c2326aa8cfba02a0171a5d7bd52bc37cdc572
AlbertBS2 <abargallos@gmail.com>
2024-12-19 20:16:26 +0100

    Feat: Add to rotate only specific pages

--- app_rotate_pdf.py
+++ app_rotate_pdf.py
@@ -2,14 +2,25 @@ import streamlit as st
 from PyPDF2 import PdfReader, PdfWriter
 from io import BytesIO
 
-def rotate_pdf(pdf_file, deg):
+####################################################################### FUNCTIONS #######################################################################
+
+def rotate_pdf(pdf_file, deg, range_pages=None):
     pdf_reader = PdfReader(pdf_file)
     pdf_writer = PdfWriter()
 
-    # Rotate each page in the PDF file
+    # Rotate pages in the PDF file
+    i=1
     for page in pdf_reader.pages:
-        page.rotate(deg)
-        pdf_writer.add_page(page)
+        if range_pages is None:
+            page.rotate(deg)
+            pdf_writer.add_page(page)
+        else:
+            if i in range(range_pages[0], range_pages[1]+1):
+                page.rotate(deg)
+                pdf_writer.add_page(page)
+            else:
+                pdf_writer.add_page(page)
+        i += 1
 
     # Save the rotated PDF file to a BytesIO object
     output = BytesIO()
@@ -21,12 +32,19 @@ def rotate_pdf(pdf_file, deg):
     return output
 
 
+def count_pages(pdf_file):
+    pdf_reader = PdfReader(pdf_file)
+    return len(pdf_reader.pages)
+
+
+####################################################################### MAIN #######################################################################
+
 def main():
     st.set_page_config(page_title='PDF Rotator', page_icon=':page_with_curl:')
 
     st.title('PDF Rotator', )
-    st.write('This app rotates a PDF file.')
 
+    # Input rotation angle
     deg = st.number_input('Rotation angle in degrees (clockwise)', value=90, min_value=-360, max_value=360, step=90)
     if deg == 0:
         st.warning('Please enter a non-zero rotation angle.')
@@ -34,10 +52,18 @@ def main():
     # Upload PDF file
     pdf_file = st.file_uploader('Upload your PDF file', type=['pdf'], accept_multiple_files=False)
 
+    # Input the range of pages to rotate
+    if pdf_file is not None:
+        num_pages = count_pages(pdf_file)
+        if num_pages > 1:
+            range_pages = st.slider('Select the range of pages to rotate', min_value=1, max_value=num_pages, value=(1, num_pages))
+        else:
+            range_pages = None
+
     if st.button("Rotate PDF", ):
         with st.spinner('Rotating PDF...'):
             if pdf_file is not None:
-                pdf_out = rotate_pdf(pdf_file, int(deg))
+                pdf_out = rotate_pdf(pdf_file, int(deg), range_pages)
 
                 # Download the rotated PDF file
                 st.download_button(
